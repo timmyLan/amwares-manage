68e6d36dec1285ebbbad3efc0ed689b10009cc45
llan <15814501057@163.com>
2016-07-06 16:10:30 +0800

    修改eslint

--- public/app/components/children/infoDialog.js
+++ public/app/components/children/infoDialog.js
@@ -3,87 +3,90 @@
  */
 import Dialog from 'material-ui/Dialog';
 import RaisedButton from 'material-ui/RaisedButton';
-import {FormsyText} from 'formsy-material-ui/lib';
+import { FormsyText } from 'formsy-material-ui/lib';
 const styles = {
-    customContentStyle : {
-        width: '75%',
-        maxWidth: 'none'
-    },
-    btnStyle : {
-        marginRight : '1em',
-        float : 'right',
-        marginTop　: '1em'
-    }
+  customContentStyle: {
+    width: '75%',
+    maxWidth: 'none'
+  },
+  btnStyle: {
+    marginRight: '1em',
+    float: 'right',
+    marginTop: '1em'
+  }
 };
 
 export default class InfoDialog extends React.Component {
-    render() {
-        let handleClose = () => this.props.actions.infoClose();
-        let createChild = () => {
-            let formData = new FormData(document.getElementById('childrenInfoForm'));
-            let data = {};
-            for (var [key, value] of formData.entries()) {
-                data[key] = value;
-            }
-            if(this.props.infoDialogState.title == 'Edit'){
-                data = {...data,id:this.props.infoDialogState.row._id,page:this.props.tableInfo.page};
-                this.props.actions.updateChild(data);
-            }else if(this.props.infoDialogState.title == 'Create'){
-                this.props.actions.createChild(data);
-            }
-            handleClose();
+  render() {
+    let handleClose = () => this.props.actions.infoClose();
+    let createChild = () => {
+      let formData = new FormData(document.getElementById('childrenInfoForm'));
+      let data = {};
+      for (let [key, value] of formData.entries()) {
+        data[key] = value;
+      }
+      if (this.props.infoDialogState.title == 'Edit') {
+        data = {
+          ...data,
+          id: this.props.infoDialogState.row._id,
+          page: this.props.tableInfo.page
         };
-        const  errorMessages = {
-            wordsError: "Please only use letters",
-            numericError: "Please provide a number",
-            urlError: "Please provide a valid URL"
-        };
-        let { wordsError, numericError, urlError } = errorMessages;
-        return (
-            <div>
+        this.props.actions.updateChild(data);
+      } else if (this.props.infoDialogState.title == 'Create') {
+        this.props.actions.createChild(data);
+      }
+      handleClose();
+    };
+    const errorMessages = {
+      wordsError: "Please only use letters",
+      numericError: "Please provide a number"
+    };
+    let {wordsError, numericError} = errorMessages;
+    return (
+      <div>
                 <Dialog
-                    title={this.props.infoDialogState.title}
-                    modal={true}
-                    contentStyle={styles.customContentStyle}
-                    open={this.props.infoDialogState.open}
-                >
+      title={this.props.infoDialogState.title}
+      modal={true}
+      contentStyle={styles.customContentStyle}
+      open={this.props.infoDialogState.open}
+      >
                     <Formsy.Form
-                        id="childrenInfoForm"
-                        onValidSubmit={createChild}
-                    >
+      id="childrenInfoForm"
+      onValidSubmit={createChild}
+      >
                         <FormsyText
-                            name="name"
-                            validations="isWords"
-                            validationError={wordsError}
-                            required
-                            hintText="What is your name?"
-                            floatingLabelText="Name"
-                            fullWidth={true}
-                            defaultValue={this.props.infoDialogState.row ? this.props.infoDialogState.row.name : ''}
-                        /><br/>
+      name="name"
+      validations="isWords"
+      validationError={wordsError}
+      required
+      hintText="What is your name?"
+      floatingLabelText="Name"
+      fullWidth={true}
+      defaultValue={this.props.infoDialogState.row ? this.props.infoDialogState.row.name : ''}
+      /><br/>
                         <FormsyText
-                            name="age"
-                            validations="isNumeric"
-                            validationError={numericError}
-                            required
-                            hintText="How old are you?"
-                            floatingLabelText="age"
-                            fullWidth={true}
-                            defaultValue={this.props.infoDialogState.row ? this.props.infoDialogState.row.age : ''}
-                        /><br/>
+      name="age"
+      validations="isNumeric"
+      validationError={numericError}
+      required
+      hintText="How old are you?"
+      floatingLabelText="age"
+      fullWidth={true}
+      defaultValue={this.props.infoDialogState.row ? this.props.infoDialogState.row.age : ''}
+      /><br/>
                         <RaisedButton
-                            label="Submit"
-                            type="submit"
-                            style={styles.btnStyle}
-                            primary={true}/>
+      label="Submit"
+      type="submit"
+      style={styles.btnStyle}
+      primary={true}/>
                         <RaisedButton
-                            label="Cancel"
-                            onTouchTap={handleClose}
-                            style={styles.btnStyle}
-                        />
+      label="Cancel"
+      onTouchTap={handleClose}
+      style={styles.btnStyle}
+      />
                     </Formsy.Form>
                 </Dialog>
             </div>
-        );
-    }
+      );
+  }
 }
